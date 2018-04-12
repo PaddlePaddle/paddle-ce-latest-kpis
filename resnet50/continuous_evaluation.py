8728f5731209a52ef9aefad291ad870c42685599
kolinwei <331911734@qq.com>
2018-04-12 16:24:55 +0800

    change the diff ratio to 5%

--- resnet50/continuous_evaluation.py
+++ resnet50/continuous_evaluation.py
@@ -5,10 +5,10 @@ from kpi import CostKpi, DurationKpi, AccKpi
 
 cifar10_128_train_acc_kpi = AccKpi('cifar10_128_train_acc', 0.05, 0)
 cifar10_128_train_speed_kpi = AccKpi('cifar10_128_train_speed', 0.05, 0)
-cifar10_128_gpu_memory_kpi = DurationKpi('cifar10_128_gpu_memory', 0.01, 0)
+cifar10_128_gpu_memory_kpi = DurationKpi('cifar10_128_gpu_memory', 0.05, 0)
 
 flowers_64_train_speed_kpi = AccKpi('flowers_64_train_speed', 0.05, 0)
-flowers_64_gpu_memory_kpi = DurationKpi('flowers_64_gpu_memory', 0.01, 0)
+flowers_64_gpu_memory_kpi = DurationKpi('flowers_64_gpu_memory', 0.05, 0)
 
 tracking_kpis = [
     cifar10_128_train_acc_kpi,
