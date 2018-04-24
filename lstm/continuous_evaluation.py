b6478aa8b55889e03d2e43318963545a4f7a0c01
kolinwei <331911734@qq.com>
2018-04-24 11:23:37 +0800

    add lstm

---
+++ lstm/continuous_evaluation.py
@@ -0,0 +1,18 @@
+"""
+continuous_evaluation.py
+"""
+import os
+import sys
+sys.path.append(os.environ['ceroot'])
+from kpi import AccKpi
+from kpi import CostKpi
+from kpi import DurationKpi
+
+imdb_32_train_speed_kpi = AccKpi('imdb_32_train_speed', 0.05, 0)
+imdb_32_gpu_memory_kpi = DurationKpi('imdb_32_gpu_memory', 0.02, 0)
+
+
+tracking_kpis = [
+    imdb_32_train_speed_kpi,
+    imdb_32_gpu_memory_kpi,
+]
