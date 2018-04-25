659ecd85f8a6b7ebff55c244726def06ae049a74
kolinwei <331911734@qq.com>
2018-04-25 17:12:59 +0800

    add transformer

--- ocr_recognition/continuous_evaluation.py
+++ ocr_recognition/continuous_evaluation.py
@@ -3,8 +3,8 @@ import sys
 sys.path.append(os.environ['ceroot'])
 from kpi import CostKpi, DurationKpi, AccKpi
 
-train_avg_loss_kpi = AccKpi('train_avg_loss', 0.05, 0)
-train_seq_err_kpi = AccKpi('train_seq_err', 0.05, 0)
+train_avg_loss_kpi = CostKpi('train_avg_loss', 0.05, 0)
+train_seq_err_kpi = CostKpi('train_seq_err', 0.05, 0)
 
 
 tracking_kpis = [
