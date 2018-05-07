d121b0939b29c751b822693f68020045a081a02d
kolinwei <331911734@qq.com>
2018-05-07 14:57:07 +0800

    active model lstm

--- lstm/continuous_evaluation.py
+++ lstm/continuous_evaluation.py
@@ -8,8 +8,8 @@ from kpi import AccKpi
 from kpi import CostKpi
 from kpi import DurationKpi
 
-imdb_32_train_speed_kpi = AccKpi('imdb_32_train_speed', 0.2, 0)
-imdb_32_gpu_memory_kpi = DurationKpi('imdb_32_gpu_memory', 0.2, 0)
+imdb_32_train_speed_kpi = AccKpi('imdb_32_train_speed', 0.03, 0, actived=True)
+imdb_32_gpu_memory_kpi = DurationKpi('imdb_32_gpu_memory', 0.05, 0, actived=True)
 
 tracking_kpis = [
     imdb_32_train_speed_kpi,
