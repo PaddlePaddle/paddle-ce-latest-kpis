2ed24394511c4e9d11468b1042507a32a605d493
kolinwei <331911734@qq.com>
2018-05-07 18:49:33 +0800

    active sequence_tagging_for_ner

--- sequence_tagging_for_ner/continuous_evaluation.py
+++ sequence_tagging_for_ner/continuous_evaluation.py
@@ -8,7 +8,7 @@ from kpi import AccKpi
 from kpi import DurationKpi
 
 train_acc_kpi = AccKpi('train_acc', 0.2, 0)
-pass_duration_kpi = DurationKpi('pass_duration', 0.2, 0)
+pass_duration_kpi = DurationKpi('pass_duration', 0.02, 0, actived=True)
 
 tracking_kpis = [
     train_acc_kpi,
