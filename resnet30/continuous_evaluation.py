d24c010b0c806d92ebea637434a152952b5b0e93
guochaorong <guochaorong@baidu.com>
2018-05-16 20:21:24 +0800

    fix nan digital for CPU

--- resnet30/continuous_evaluation.py
+++ resnet30/continuous_evaluation.py
@@ -3,11 +3,11 @@ import sys
 sys.path.append(os.environ['ceroot'])
 from kpi import CostKpi, AccKpi, DurationKpi
 
-train_cost_kpi = CostKpi('train_cost', 0.05, actived=True)
-train_acc_kpi = AccKpi('train_acc', 0.02, actived=True)  
-test_acc_kpi = AccKpi('test_acc', 0.05, actived=True)  
-train_speed_kpi = AccKpi('train_speed', 0.01, actived=True)  
-train_duration_kpi = DurationKpi('train_duration', 0.02, actived=True)
+train_cost_kpi = CostKpi('train_cost', 0.05, 0, actived=True)
+train_acc_kpi = AccKpi('train_acc', 0.02, 0, actived=True)  
+test_acc_kpi = AccKpi('test_acc', 0.05, 0, actived=True)  
+train_speed_kpi = AccKpi('train_speed', 0.01, 0, actived=True)  
+train_duration_kpi = DurationKpi('train_duration', 0.02, 0, actived=True)
 
 
 tracking_kpis = [
