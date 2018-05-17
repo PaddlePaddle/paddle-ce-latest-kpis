4fde36406e1db9fff4a30d494c2e934392a3a2fd
kolinwei <331911734@qq.com>
2018-05-17 18:31:03 +0800

    image_classification add four card kpi

--- image_classification/continuous_evaluation.py
+++ image_classification/continuous_evaluation.py
@@ -3,8 +3,26 @@ import sys
 sys.path.append(os.environ['ceroot'])
 from kpi import CostKpi, DurationKpi, AccKpi
 
-train_acc_top1_kpi = AccKpi('train_acc_top1_kpi', 0.2, 0)
-train_acc_top5_kpi = AccKpi('train_acc_top5_kpi', 0.2, 0)
-train_cost_kpi = CostKpi('train_cost_kpi', 0.2, 0)
+train_acc_top1_kpi = AccKpi('train_acc_top1_kpi', 0.05, 0,
+                            actived=True,
+                            desc='TOP1 ACC')
+train_acc_top5_kpi = AccKpi('train_acc_top5_kpi', 0.05, 0,
+                            actived=True,
+                            desc='TOP5 ACC')
+train_cost_kpi = CostKpi('train_cost_kpi', 0.05, 0,
+                        actived=True,
+                        desc='train cost')
+train_speed_kpi = AccKpi('train_speed_kpi', 0.05, 0,
+                        actived=True,
+                        unit_repr='images/s',
+                        desc='train speed in one GPU card')
+four_card_train_speed_kpi = AccKpi('four_card_train_speed_kpi', 0.05, 0,
+                        actived=True,
+                        unit_repr='images/s',
+                        desc='train speed in four GPU card')
 
-tracking_kpis = [train_acc_top1_kpi, train_acc_top5_kpi, train_cost_kpi]
+tracking_kpis = [train_acc_top1_kpi, 
+                train_acc_top5_kpi, 
+                train_cost_kpi,
+                train_speed_kpi,
+                four_card_train_speed_kpi]
