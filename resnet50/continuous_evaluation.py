fa2c55e1ed1aafe0923d59b30783d05f6a0d7983
guochaorong <guochaorong@baidu.com>
2018-06-10 23:35:29 +0800

    add 4 card and 8 card for resnet50

--- resnet50/continuous_evaluation.py
+++ resnet50/continuous_evaluation.py
@@ -5,15 +5,15 @@ from kpi import CostKpi, DurationKpi, AccKpi
 
 cifar10_train_acc_kpi = AccKpi('cifar10_train_acc', 0.02, 0, actived=True)
 cifar10_train_speed_kpi = AccKpi('cifar10_train_speed', 0.02, 0, actived=True)
-cifar10_gpu_memory_kpi = DurationKpi('cifar10_gpu_memory', 0.02, 0, actived=True)
+cifar10_gpu_memory_kpi = DurationKpi('cifar10_gpu_memory', 0.02, 0)
 flowers_train_speed_kpi = AccKpi('flowers_train_speed', 0.02, 0, actived=True)
-flowers_gpu_memory_kpi = DurationKpi('flowers_gpu_memory', 0.02, 0, actived=True)
+flowers_gpu_memory_kpi = DurationKpi('flowers_gpu_memory', 0.02, 0)
 
 cifar10_train_acc_kpi_card4 = AccKpi('cifar10_train_acc_card4', 0.02, 0, actived=True)
 cifar10_train_speed_kpi_card4 = AccKpi('cifar10_train_speed_card4', 0.02, 0, actived=True)
-cifar10_gpu_memory_kpi_card4 = DurationKpi('cifar10_gpu_memory_card4', 0.02, 0, actived=True)
+cifar10_gpu_memory_kpi_card4 = DurationKpi('cifar10_gpu_memory_card4', 0.02, 0)
 flowers_train_speed_kpi_card4 = AccKpi('flowers_train_speed_card4', 0.02, 0, actived=True)
-flowers_gpu_memory_kpi_card4 = DurationKpi('flowers_gpu_memory_card4', 0.02, 0, actived=True)
+flowers_gpu_memory_kpi_card4 = DurationKpi('flowers_gpu_memory_card4', 0.02, 0)
 
 cifar10_train_acc_kpi_card8 = AccKpi('cifar10_train_acc_card8', 0.02, 0, actived=True)
 cifar10_train_speed_kpi_card8 = AccKpi('cifar10_train_speed_card8', 0.02, 0, actived=True)
--- resnet50/run.xsh
+++ resnet50/run.xsh
@@ -8,6 +8,8 @@ arr[1]='0,1,2,3'
 if [[ `nvidia-smi  --list-gpus| wc -l` = 8 ]]; then
     #agent has 4 card no need to run 8 card scene
     arr[2]='0,1,2,3,4,5,6,7'
+else 
+    cp -rf latest_kpis/*card8* ../ 
 fi
 
 for var in ${arr[@]};
@@ -18,7 +20,7 @@ do
     echo $CUDA_VISIBLE_DEVICES
 
     #FLAGS_benchmark=true FLAGS_fraction_of_gpu_memory_to_use=0.0 
-    python model.py --device=GPU --batch_size=128 --data_set=cifar10 --model=resnet_cifar10 --pass_num=30  --gpu_id $cudaid
+    python model.py --device=GPU --batch_size=128 --data_set=cifar10 --model=resnet_cifar10 --pass_num=3  --gpu_id $cudaid
 
     #FLAGS_benchmark=true FLAGS_fraction_of_gpu_memory_to_use=0.0 
     python model.py --device=GPU --batch_size=32 --data_set=flowers --model=resnet_imagenet --pass_num=3  --use_fake_data --gpu_id $cudaid --iterations 20
