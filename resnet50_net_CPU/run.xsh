8bf1ebe3eef1182c931b0b0f3fc9cd70c08c9b40
chengduoZH <zhaochengduo@163.com>
2018-07-20 18:48:06 +0800

    Move get_gpu_data.py to seperate file

--- resnet50_net_CPU/run.xsh
+++ resnet50_net_CPU/run.xsh
@@ -8,8 +8,12 @@ export OMP_NUM_THREADS=1
 export CPU_NUM=4
 python train.py --use_gpu=false --reduce_strategy="AllReduce" --batch_size=8 --model=resnet_cifar10  --pass_num=5 
 python train.py --use_gpu=false --reduce_strategy="Reduce" --batch_size=8 --model=resnet_cifar10  --pass_num=5 
+#flowers 16
+python train.py --use_gpu=false --reduce_strategy="AllReduce" --batch_size=8 --model=resnet_imagenet --pass_num=5 
+python train.py --use_gpu=false --reduce_strategy="Reduce" --batch_size=8 --model=resnet_imagenet --pass_num=5 
 
 # CPU Test
 # cifar10 16
 export CPU_NUM=1
 python train.py --use_gpu=false --batch_size=8 --model=resnet_cifar10  --pass_num=5 
+python train.py --use_gpu=false --batch_size=8 --model=resnet_imagenet --pass_num=5
--- resnet50_net_GPU/continuous_evaluation.py
+++ resnet50_net_GPU/continuous_evaluation.py
@@ -6,6 +6,28 @@ sys.path.append(os.environ['ceroot'])
 from kpi import AccKpi
 from kpi import DurationKpi
 
+cifar10_128_AllReduce_GPU_4_Cards_train_acc_kpi = AccKpi(
+    'cifar10_128_AllReduce_GPU_4_Cards_train_acc', 0.03, 0, actived=True)
+cifar10_128_AllReduce_GPU_4_Cards_train_speed_kpi = AccKpi(
+    'cifar10_128_AllReduce_GPU_4_Cards_train_speed', 0.06, 0, actived=True)
+cifar10_128_AllReduce_4_Cards_gpu_memory_kpi = DurationKpi(
+    'cifar10_128_AllReduce_4_Cards_gpu_memory', 0.1, 0, actived=True)
+
+cifar10_128_Reduce_GPU_4_Cards_train_acc_kpi = AccKpi(
+    'cifar10_128_Reduce_GPU_4_Cards_train_acc', 0.03, 0, actived=True)
+cifar10_128_Reduce_GPU_4_Cards_train_speed_kpi = AccKpi(
+    'cifar10_128_Reduce_GPU_4_Cards_train_speed', 0.06, 0, actived=True)
+cifar10_128_Reduce_4_Cards_gpu_memory_kpi = DurationKpi(
+    'cifar10_128_Reduce_4_Cards_gpu_memory', 0.1, 0, actived=True)
+
+# Single Cards
+cifar10_128_GPU_1_Cards_train_acc_kpi = AccKpi(
+    'cifar10_128_GPU_1_Cards_train_acc', 0.03, 0, actived=True)
+cifar10_128_GPU_1_Cards_train_speed_kpi = AccKpi(
+    'cifar10_128_GPU_1_Cards_train_speed', 0.06, 0, actived=True)
+cifar10_128_1_Cards_gpu_memory_kpi = DurationKpi(
+    'cifar10_128_1_Cards_gpu_memory', 0.1, 0, actived=True)
+
 flowers_64_AllReduce_GPU_4_Cards_train_acc_kpi = AccKpi(
     'flowers_64_AllReduce_GPU_4_Cards_train_acc', 0.03, 0, actived=True)
 flowers_64_AllReduce_GPU_4_Cards_train_speed_kpi = AccKpi(
@@ -28,6 +50,15 @@ flowers_64_1_Cards_gpu_memory_kpi = DurationKpi(
     'flowers_64_1_Cards_gpu_memory', 0.1, 0, actived=True)
 
 tracking_kpis = [
+    cifar10_128_AllReduce_GPU_4_Cards_train_acc_kpi,
+    cifar10_128_AllReduce_GPU_4_Cards_train_speed_kpi,
+    cifar10_128_AllReduce_4_Cards_gpu_memory_kpi,
+    cifar10_128_Reduce_GPU_4_Cards_train_acc_kpi,
+    cifar10_128_Reduce_GPU_4_Cards_train_speed_kpi,
+    cifar10_128_Reduce_4_Cards_gpu_memory_kpi,
+    cifar10_128_GPU_1_Cards_train_acc_kpi,
+    cifar10_128_GPU_1_Cards_train_speed_kpi,
+    cifar10_128_1_Cards_gpu_memory_kpi,
     flowers_64_AllReduce_GPU_4_Cards_train_acc_kpi,
     flowers_64_AllReduce_GPU_4_Cards_train_speed_kpi,
     flowers_64_AllReduce_4_Cards_gpu_memory_kpi,
