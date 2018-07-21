00d38f11ccc59df3308c0ea76e41b38362330ebc
chengduoZH <zhaochengduo@163.com>
2018-07-21 22:42:26 +0800

    inactive

---
+++ resnet50_net/continuous_evaluation.py
@@ -0,0 +1,9 @@
+import os
+import sys
+
+sys.path.append(os.environ['ceroot'])
+
+from kpi import AccKpi
+from kpi import DurationKpi
+
+tracking_kpis = []
--- resnet50_net_CPU/continuous_evaluation.py
+++ resnet50_net_CPU/continuous_evaluation.py
@@ -7,34 +7,34 @@ from kpi import AccKpi
 from kpi import DurationKpi
 
 cifar10_8_AllReduce_CPU_4_Cards_train_acc_kpi = AccKpi(
-    'cifar10_8_AllReduce_CPU_4_Cards_train_acc', 0.03, 0, actived=True)
+    'cifar10_8_AllReduce_CPU_4_Cards_train_acc', 0.03, 0, actived=False)
 cifar10_8_AllReduce_CPU_4_Cards_train_speed_kpi = AccKpi(
-    'cifar10_8_AllReduce_CPU_4_Cards_train_speed', 0.06, 0, actived=True)
+    'cifar10_8_AllReduce_CPU_4_Cards_train_speed', 0.06, 0, actived=False)
 
 cifar10_8_Reduce_CPU_4_Cards_train_acc_kpi = AccKpi(
-    'cifar10_8_Reduce_CPU_4_Cards_train_acc', 0.03, 0, actived=True)
+    'cifar10_8_Reduce_CPU_4_Cards_train_acc', 0.03, 0, actived=False)
 cifar10_8_Reduce_CPU_4_Cards_train_speed_kpi = AccKpi(
-    'cifar10_8_Reduce_CPU_4_Cards_train_speed', 0.06, 0, actived=True)
+    'cifar10_8_Reduce_CPU_4_Cards_train_speed', 0.06, 0, actived=False)
 
 cifar10_8_CPU_1_Cards_train_acc_kpi = AccKpi(
-    'cifar10_8_CPU_1_Cards_train_acc', 0.03, 0, actived=True)
+    'cifar10_8_CPU_1_Cards_train_acc', 0.03, 0, actived=False)
 cifar10_8_CPU_1_Cards_train_speed_kpi = AccKpi(
-    'cifar10_8_CPU_1_Cards_train_speed', 0.06, 0, actived=True)
+    'cifar10_8_CPU_1_Cards_train_speed', 0.06, 0, actived=False)
 
 flowers_8_AllReduce_CPU_4_Cards_train_acc_kpi = AccKpi(
-    'flowers_8_AllReduce_CPU_4_Cards_train_acc', 0.03, 0, actived=True)
+    'flowers_8_AllReduce_CPU_4_Cards_train_acc', 0.03, 0, actived=False)
 flowers_8_AllReduce_CPU_4_Cards_train_speed_kpi = AccKpi(
-    'flowers_8_AllReduce_CPU_4_Cards_train_speed', 0.06, 0, actived=True)
+    'flowers_8_AllReduce_CPU_4_Cards_train_speed', 0.06, 0, actived=False)
 
 flowers_8_Reduce_CPU_4_Cards_train_acc_kpi = AccKpi(
-    'flowers_8_Reduce_CPU_4_Cards_train_acc', 0.03, 0, actived=True)
+    'flowers_8_Reduce_CPU_4_Cards_train_acc', 0.03, 0, actived=False)
 flowers_8_Reduce_CPU_4_Cards_train_speed_kpi = AccKpi(
-    'flowers_8_Reduce_CPU_4_Cards_train_speed', 0.06, 0, actived=True)
+    'flowers_8_Reduce_CPU_4_Cards_train_speed', 0.06, 0, actived=False)
 
 flowers_8_CPU_1_Cards_train_acc_kpi = AccKpi(
-    'flowers_8_CPU_1_Cards_train_acc', 0.03, 0, actived=True)
+    'flowers_8_CPU_1_Cards_train_acc', 0.03, 0, actived=False)
 flowers_8_CPU_1_Cards_train_speed_kpi = AccKpi(
-    'flowers_8_CPU_1_Cards_train_speed', 0.06, 0, actived=True)
+    'flowers_8_CPU_1_Cards_train_speed', 0.06, 0, actived=False)
 
 tracking_kpis = [
     cifar10_8_AllReduce_CPU_4_Cards_train_acc_kpi,
