4c23fa1d4a513fefbb6c5c3ca4a828bf4739fd8e
chengduoZH <zhaochengduo@163.com>
2018-07-24 08:47:50 +0800

    add link

---
+++ resnet50_net_CPU/continuous_evaluation.py
@@ -0,0 +1,52 @@
+import os
+import sys
+
+sys.path.append(os.environ['ceroot'])
+
+from kpi import AccKpi
+from kpi import DurationKpi
+
+cifar10_8_AllReduce_CPU_4_Cards_train_acc_kpi = AccKpi(
+    'cifar10_8_AllReduce_CPU_4_Cards_train_acc', 0.03, 0, actived=True)
+cifar10_8_AllReduce_CPU_4_Cards_train_speed_kpi = AccKpi(
+    'cifar10_8_AllReduce_CPU_4_Cards_train_speed', 0.06, 0, actived=True)
+
+cifar10_8_Reduce_CPU_4_Cards_train_acc_kpi = AccKpi(
+    'cifar10_8_Reduce_CPU_4_Cards_train_acc', 0.03, 0, actived=False)
+cifar10_8_Reduce_CPU_4_Cards_train_speed_kpi = AccKpi(
+    'cifar10_8_Reduce_CPU_4_Cards_train_speed', 0.06, 0, actived=True)
+
+cifar10_8_CPU_1_Cards_train_acc_kpi = AccKpi(
+    'cifar10_8_CPU_1_Cards_train_acc', 0.03, 0, actived=True)
+cifar10_8_CPU_1_Cards_train_speed_kpi = AccKpi(
+    'cifar10_8_CPU_1_Cards_train_speed', 0.06, 0, actived=True)
+
+flowers_8_AllReduce_CPU_4_Cards_train_acc_kpi = AccKpi(
+    'flowers_8_AllReduce_CPU_4_Cards_train_acc', 0.03, 0, actived=False)
+flowers_8_AllReduce_CPU_4_Cards_train_speed_kpi = AccKpi(
+    'flowers_8_AllReduce_CPU_4_Cards_train_speed', 0.06, 0, actived=True)
+
+flowers_8_Reduce_CPU_4_Cards_train_acc_kpi = AccKpi(
+    'flowers_8_Reduce_CPU_4_Cards_train_acc', 0.03, 0, actived=False)
+flowers_8_Reduce_CPU_4_Cards_train_speed_kpi = AccKpi(
+    'flowers_8_Reduce_CPU_4_Cards_train_speed', 0.06, 0, actived=True)
+
+flowers_8_CPU_1_Cards_train_acc_kpi = AccKpi(
+    'flowers_8_CPU_1_Cards_train_acc', 0.03, 0, actived=False)
+flowers_8_CPU_1_Cards_train_speed_kpi = AccKpi(
+    'flowers_8_CPU_1_Cards_train_speed', 0.06, 0, actived=True)
+
+tracking_kpis = [
+    cifar10_8_AllReduce_CPU_4_Cards_train_acc_kpi,
+    cifar10_8_AllReduce_CPU_4_Cards_train_speed_kpi,
+    cifar10_8_Reduce_CPU_4_Cards_train_acc_kpi,
+    cifar10_8_Reduce_CPU_4_Cards_train_speed_kpi,
+    cifar10_8_CPU_1_Cards_train_acc_kpi,
+    cifar10_8_CPU_1_Cards_train_speed_kpi,
+    flowers_8_AllReduce_CPU_4_Cards_train_acc_kpi,
+    flowers_8_AllReduce_CPU_4_Cards_train_speed_kpi,
+    flowers_8_Reduce_CPU_4_Cards_train_acc_kpi,
+    flowers_8_Reduce_CPU_4_Cards_train_speed_kpi,
+    flowers_8_CPU_1_Cards_train_acc_kpi,
+    flowers_8_CPU_1_Cards_train_speed_kpi,
+]
