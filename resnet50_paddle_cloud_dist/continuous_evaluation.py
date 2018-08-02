df5c06aff9a93a282566d34600d982ba308f2b6b
minqiyang <minqiyang@baidu.com>
2018-08-02 16:41:39 +0800

    Fix acc and speed value

--- resnet50_paddle_cloud_dist/continuous_evaluation.py
+++ resnet50_paddle_cloud_dist/continuous_evaluation.py
@@ -3,8 +3,8 @@ import sys
 sys.path.append(os.environ['ceroot'])
 from kpi import AccKpi
 
-flowers_resnet50_dist_train_acc_kpi = AccKpi('flowers_resnet50_dist_train_acc', 0.02, 0, actived=True)
-flowers_resnet50_dist_train_speed_kpi = AccKpi('flowers_resnet50_dist_train_speed', 0.02, 0, actived=True)
+flowers_resnet50_dist_train_acc_kpi = AccKpi('flowers_resnet50_dist_train_acc', 0.08, 0, actived=True)
+flowers_resnet50_dist_train_speed_kpi = AccKpi('flowers_resnet50_dist_train_speed', 0.1, 0, actived=True)
 
 tracking_kpis = [
     flowers_resnet50_dist_train_speed_kpi,
