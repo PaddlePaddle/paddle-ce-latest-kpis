7e8ade23462a156d3b76d0b94115e7711a2b5ff7
chengduoZH <zhaochengduo@163.com>
2018-08-27 19:54:07 +0800

    reset kpi

--- resnet50_net_GPU/continuous_evaluation.py
+++ resnet50_net_GPU/continuous_evaluation.py
@@ -31,14 +31,14 @@ cifar10_128_1_Cards_gpu_memory_kpi = DurationKpi(
 flowers_64_AllReduce_GPU_4_Cards_train_acc_kpi = AccKpi(
     'flowers_64_AllReduce_GPU_4_Cards_train_acc', 0.03, 0, actived=True)
 flowers_64_AllReduce_GPU_4_Cards_train_speed_kpi = AccKpi(
-    'flowers_64_AllReduce_GPU_4_Cards_train_speed', 0.09, 0, actived=True)
+    'flowers_64_AllReduce_GPU_4_Cards_train_speed', 0.10, 0, actived=True)
 flowers_64_AllReduce_4_Cards_gpu_memory_kpi = DurationKpi(
     'flowers_64_AllReduce_4_Cards_gpu_memory', 0.1, 0, actived=True)
 
 flowers_64_Reduce_GPU_4_Cards_train_acc_kpi = AccKpi(
     'flowers_64_Reduce_GPU_4_Cards_train_acc', 0.03, 0, actived=True)
 flowers_64_Reduce_GPU_4_Cards_train_speed_kpi = AccKpi(
-    'flowers_64_Reduce_GPU_4_Cards_train_speed', 0.06, 0, actived=True)
+    'flowers_64_Reduce_GPU_4_Cards_train_speed', 0.09, 0, actived=True)
 flowers_64_Reduce_4_Cards_gpu_memory_kpi = DurationKpi(
     'flowers_64_Reduce_4_Cards_gpu_memory', 0.1, 0, actived=True)
 
@@ -46,7 +46,7 @@ flowers_64_Reduce_4_Cards_gpu_memory_kpi = DurationKpi(
 flowers_64_GPU_1_Cards_train_acc_kpi = AccKpi(
     'flowers_64_GPU_1_Cards_train_acc', 0.03, 0, actived=True)
 flowers_64_GPU_1_Cards_train_speed_kpi = AccKpi(
-    'flowers_64_GPU_1_Cards_train_speed', 0.06, 0, actived=True)
+    'flowers_64_GPU_1_Cards_train_speed', 0.09, 0, actived=True)
 flowers_64_1_Cards_gpu_memory_kpi = DurationKpi(
     'flowers_64_1_Cards_gpu_memory', 0.1, 0, actived=True)
 
