c72c31780c8f51fe26a8f956dccf31f3db8dfb54
chengduoZH <zhaochengduo@163.com>
2018-08-28 12:34:13 +0800

    reset rate ratio

--- resnet50_net_CPU/continuous_evaluation.py
+++ resnet50_net_CPU/continuous_evaluation.py
@@ -32,7 +32,7 @@ flowers_8_Reduce_CPU_4_Cards_train_speed_kpi = AccKpi(
     'flowers_8_Reduce_CPU_4_Cards_train_speed', 0.06, 0, actived=True)
 
 flowers_8_CPU_1_Cards_train_acc_kpi = AccKpi(
-    'flowers_8_CPU_1_Cards_train_acc', 0.03, 0, actived=True)
+    'flowers_8_CPU_1_Cards_train_acc', 0.05, 0, actived=True)
 flowers_8_CPU_1_Cards_train_speed_kpi = AccKpi(
     'flowers_8_CPU_1_Cards_train_speed', 0.06, 0, actived=True)
 
