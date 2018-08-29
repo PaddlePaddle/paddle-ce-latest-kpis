316c3d9c8aeee32009469d3a5f51d8b7b59771fd
chengduoZH <zhaochengduo@163.com>
2018-08-29 09:29:03 +0800

    reset kpi

--- resnet50_net_GPU/continuous_evaluation.py
+++ resnet50_net_GPU/continuous_evaluation.py
@@ -24,7 +24,7 @@ cifar10_128_Reduce_4_Cards_gpu_memory_kpi = DurationKpi(
 cifar10_128_GPU_1_Cards_train_acc_kpi = AccKpi(
     'cifar10_128_GPU_1_Cards_train_acc', 0.03, 0, actived=True)
 cifar10_128_GPU_1_Cards_train_speed_kpi = AccKpi(
-    'cifar10_128_GPU_1_Cards_train_speed', 0.06, 0, actived=True)
+    'cifar10_128_GPU_1_Cards_train_speed', 0.10, 0, actived=True)
 cifar10_128_1_Cards_gpu_memory_kpi = DurationKpi(
     'cifar10_128_1_Cards_gpu_memory', 0.1, 0, actived=True)
 
