da3978190b6898f5e8353e4920fe52f632ea43ec
kolinwei <331911734@qq.com>
2018-12-24 15:12:24 +0800

    Update continuous_evaluation.py

--- object_detection/continuous_evaluation.py
+++ object_detection/continuous_evaluation.py
@@ -4,7 +4,7 @@ sys.path.append(os.environ['ceroot'])
 from kpi import CostKpi, DurationKpi, AccKpi
 
 train_cost_kpi = CostKpi('train_cost', 0.02, 0, actived=True)
-train_speed_kpi = AccKpi('train_speed', 0.03, 0, actived=True)
+train_speed_kpi = AccKpi('train_speed', 0.05, 0, actived=True)
 four_card_speed_kpi = AccKpi('four_card_train_speed', 0.05, 0, actived=True)
 
 tracking_kpis = [train_cost_kpi, train_speed_kpi, four_card_speed_kpi]
