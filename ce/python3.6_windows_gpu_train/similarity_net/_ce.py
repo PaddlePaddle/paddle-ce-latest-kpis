8b7cbc32260b71d5a1f611dcb53d3b35a5f6d1a5
jiaxiao243@126.com <jiaxiao243@126.com>
2019-12-26 14:27:03 +0800

    update models

--- ce/python3.6_windows_gpu_train/similarity_net/_ce.py
+++ ce/python3.6_windows_gpu_train/similarity_net/_ce.py
@@ -7,8 +7,8 @@ from kpi import CostKpi
 from kpi import DurationKpi
 from kpi import AccKpi
 
-each_step_duration_simnet_card1 = DurationKpi('each_step_duration_simnet_card1', 0.03, 0, actived=False)
-train_loss_simnet_card1 = CostKpi('train_loss_simnet_card1', 0.01, 0, actived=False)
+each_step_duration_simnet_card1 = DurationKpi('each_step_duration_simnet_card1', 0.03, 0, actived=True)
+train_loss_simnet_card1 = CostKpi('train_loss_simnet_card1', 0.01, 0, actived=True)
 
 tracking_kpis = [
         each_step_duration_simnet_card1,
