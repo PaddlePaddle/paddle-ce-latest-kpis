5af640a668803f402ceb85d228eba8168aa71ab9
jiaxiao243@126.com <jiaxiao243@126.com>
2020-01-16 18:48:54 +0800

    add classification

--- ce/python3.6_windows_gpu_train/image_classification/.run_ce.bat
+++ ce/python3.6_windows_gpu_train/image_classification/.run_ce.bat
@@ -6,29 +6,32 @@ set FLAGS_fast_eager_deletion_mode=1
 set FLAGS_fraction_of_gpu_memory_to_use=0
 
 set CUDA_VISIBLE_DEVICES=0
-python train.py --model=ResNet50 --num_epochs=2 --batch_size 8 --lr_strategy=cosine_decay --pretrained_model=ResNet50_pretrained --random_seed 1000 --use_gpu true --enable_ce=True 2>log
-type log  | python _ce.py
 
+setlocal enabledelayedexpansion
+for %%I in (AlexNet DPN107 DarkNet53 DenseNet121 EfficientNet HRNet_W18_C GoogLeNet InceptionV4 Xception65_deeplab MobileNetV1 MobileNetV2 ResNet50 ResNet152_vd Res2Net50_vd_26w_4s ResNeXt101_32x4d ResNeXt101_32x8d_wsl SE_ResNeXt50_vd_32x4d ShuffleNetV2_swish SqueezeNet1_1 VGG19) do (
+python train.py --model=%%I --num_epochs=1 --batch_size 8 --lr_strategy=cosine_decay --random_seed 1000 --use_gpu true --enable_ce=True > %%I.log 2>&1
+type %%I.log | grep "train_cost_card1" | gawk "{print \"kpis\t\"\"%%I\"\"_loss_card1\t\"$5}" | python _ce.py
+type %%I.log | grep "train_speed_card1" | gawk "{print \"kpis\t\"\"%%I\"\"_time_card1\t\"$5}" | python _ce.py
 rem eval
-python eval.py --model=ResNet50 --batch_size=32 --pretrained_model=ResNet50_pretrained --use_gpu True >%log_path%/ResNet50_E.log 2>&1 
-if not %errorlevel% == 0 (
-        move  %log_path%\ResNet50_E.log  %log_path%\FAIL\ResNet50_E.log
-        echo   ResNet50,eval,FAIL  >> %log_path%\result.log
-        echo  evaling of ResNet50 failed!
+python eval.py --model=%%I --batch_size=32 --pretrained_model=output/%%I/0 --use_gpu true >%log_path%/%%I_E.log 2>&1
+if not !errorlevel! == 0 (
+        move  %log_path%\%%I_E.log  %log_path%\FAIL\%%I_E.log
+        echo   %%I,eval,FAIL  >> %log_path%\result.log
+        echo  evaling of %%I failed!
 ) else (
-        move  %log_path%\ResNet50_E.log  %log_path%\SUCCESS\ResNet50_E.log
-        echo   ResNet50,eval,SUCCESS  >> %log_path%\result.log
-        echo   evaling of ResNet50 successfully!
+        move  %log_path%\%%I_E.log  %log_path%\SUCCESS\%%I_E.log
+        echo   %%I,eval,SUCCESS  >> %log_path%\result.log
+        echo   evaling of %%I successfully!
 )
 rem infer
-python infer.py --model=ResNet50 --pretrained_model=ResNet50_pretrained --use_gpu True >%log_path%/ResNet50_I.log 2>&1
-if not %errorlevel% == 0 (
-        move  %log_path%\ResNet50_I.log  %log_path%\FAIL\ResNet50_I.log
-        echo   ResNet50,infer,FAIL  >> %log_path%\result.log
-        echo  infering of ResNet50 failed!
+python infer.py --model=%%I --pretrained_model=output/%%I/0 --use_gpu True >%log_path%/%%I_I.log 2>&1
+if not !errorlevel! == 0 (
+        move  %log_path%\%%I_I.log  %log_path%\FAIL\%%I_I.log
+        echo   %%I,infer,FAIL  >> %log_path%\result.log
+        echo  infering of %%I failed!
 ) else (
-        move  %log_path%\ResNet50_I.log  %log_path%\SUCCESS\ResNet50_I.log
-        echo   ResNet50,infer,SUCCESS  >> %log_path%\result.log
-        echo   infering of ResNet50 successfully!
+        move  %log_path%\%%I_I.log  %log_path%\SUCCESS\%%I_I.log
+        echo   %%I,infer,SUCCESS  >> %log_path%\result.log
+        echo   infering of %%I successfully!
 )
-
+)
