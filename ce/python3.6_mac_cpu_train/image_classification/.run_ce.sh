cb32d58dd732f50086f722f78d97edf7138ad359
jiaxiao243@126.com <jiaxiao243@126.com>
2020-03-11 18:56:51 +0800

    update mac models

--- ce/python3.6_mac_cpu_train/image_classification/.run_ce.sh
+++ ce/python3.6_mac_cpu_train/image_classification/.run_ce.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # This file is only used for continuous evaluation.
-export FLAGS_eager_delete_tensor_gb=0.0
-export FLAGS_fast_eager_deletion_mode=1
-export FLAGS_fraction_of_gpu_memory_to_use=0
-
-python train.py --model=ResNet50 --num_epochs=2 --batch_size 8 --lr_strategy=cosine_decay --pretrained_model=ResNet50_pretrained --random_seed 1000 --use_gpu false --enable_ce=True 2>log
-cat log  | python _ce.py
-
+model_list='AlexNet DPN107 DarkNet53 DenseNet121 EfficientNet HRNet_W18_C GoogLeNet InceptionV4 Xception65_deeplab MobileNetV1 MobileNetV2 ResNet50 ResNet152_vd Res2Net50_vd_26w_4s ResNeXt101_32x4d ResNeXt101_32x8d_wsl SE_ResNeXt50_vd_32x4d ShuffleNetV2_swish SqueezeNet1_1 VGG19'
+for model in $model_list
+do
+python train.py --model=$model --num_epochs=1 --batch_size 8 --lr_strategy=cosine_decay --random_seed 1000 --use_gpu False --enable_ce=True > $model.log 2>&1
+cat  $model.log | grep "train_cost_card1" | awk '{print "kpis\t""'$model'""_loss_card1\t"$5}' | python _ce.py
+cat  $model.log | grep "train_speed_card1" | awk '{print "kpis\t""'$model'""_time_card1\t"$5}' | python _ce.py
+done
