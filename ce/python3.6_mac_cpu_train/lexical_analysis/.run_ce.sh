c2f488d24818ae0f3a353951942ee85324210600
jiaxiao243@126.com <jiaxiao243@126.com>
2020-03-25 19:17:18 +0800

    add deepvice3

--- ce/python3.6_mac_cpu_train/lexical_analysis/.run_ce.sh
+++ ce/python3.6_mac_cpu_train/lexical_analysis/.run_ce.sh
@@ -3,7 +3,8 @@ export FLAGS_fraction_of_gpu_memory_to_use=0.5
 export FLAGS_eager_delete_tensor_gb=0.0
 export FLAGS_fast_eager_deletion_mode=1
 pip install -U paddlehub
-python train.py --train_data ./data/train.tsv --test_data ./data/test.tsv --model_save_dir ./models --validation_steps 2 --save_steps 10 --batch_size 100 --epoch 2 --use_cuda false --traindata_shuffle_buffer 200000 --word_emb_dim 768 --grnn_hidden_dim 768 --bigru_num 2  --base_learning_rate 1e-3 --emb_learning_rate 5 --crf_learning_rate 0.2 --word_dict_path ./conf/word.dic --label_dict_path ./conf/tag.dic  --word_rep_dict_path ./conf/q2b.dic --enable_ce true --random_seed  90 | python _ce.py
+python train.py --train_data ./data/train.tsv --test_data ./data/test.tsv --model_save_dir ./models --validation_steps 2 --save_steps 10 --batch_size 100 --epoch 1 --use_cuda false --traindata_shuffle_buffer 200000 --word_emb_dim 768 --grnn_hidden_dim 768 --bigru_num 2  --base_learning_rate 1e-3 --emb_learning_rate 5 --crf_learning_rate 0.2 --word_dict_path ./conf/word.dic --label_dict_path ./conf/tag.dic  --word_rep_dict_path ./conf/q2b.dic --enable_ce true --random_seed  90 | python _ce.py			
+
 # eval
 python eval.py --batch_size 200 --word_emb_dim 128 --grnn_hidden_dim 128 --bigru_num 2 --use_cuda False --init_checkpoint ./model_baseline --test_data ./data/test.tsv --word_dict_path ./conf/word.dic --label_dict_path ./conf/tag.dic --word_rep_dict_path ./conf/q2b.dic > $log_path/lac_eval.log 2>&1
 if [ $? -ne 0 ];then↩
--- ce/python3.6_windows_gpu_train/transformer_tts/run.bat
+++ ce/python3.6_windows_gpu_train/transformer_tts/run.bat
@@ -1,7 +1,7 @@
 @echo off
 set models_dir=./../../parakeet_repo
 rem copy models files
-xcopy "%models_dir%/examples/deepvoice3/." . /s /e /y /d
+xcopy "%models_dir%/examples/transformer_tts/." . /s /e /y /d
 if not exist data (mklink /j data %data_path%\DeepVoice3)
 python -c "import nltk;nltk.download('punkt');nltk.download('cmudict')"
 .\.run_ce.bat
