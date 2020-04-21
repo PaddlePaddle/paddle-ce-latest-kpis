3090c2ef7007420a1bd6a02d441b158e60e0847d
mmglove <gmm_email@163.com>
2020-04-21 10:28:31 +0800

    dete old mmoe ce

--- ce/python3.7_linux_cpu_train/mmoe_cpu/.run_ce.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# cpu
-python mmoe_train.py >mmoe_cpu_T 2>&1
-cat mmoe_cpu_T |awk -F ':| ' 'END{print "kpis\ttrain_loss_cpu\t"$4}' |tr -d '[|]|,' | python _ce.py
--- ce/python3.7_linux_cpu_train/mmoe_cpu/run.xsh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export models_dir=$PWD/../../models_repo
-
-#copy models files
-cp -r ${models_dir}/PaddleRec/multi-task/MMoE/* ./
-
-# auto data
-./.run_ce.sh
