2fdb545ca915ed246c3d2b8589f399d135f69136
Aurelius84 <zhangliujie@baidu.com>
2020-10-27 02:42:59 +0000

    rm nohup setting

--- dy2static/unittest/mnist/train.sh
+++ dy2static/unittest/mnist/train.sh
@@ -4,4 +4,4 @@ model="mnist"
 
 log_path="./${model}.log"
 
-python model.py --device=GPU --batch_size=128 --pass_num=5 > $log_path 2>&1 &
+python model.py --device=GPU --batch_size=128 --pass_num=5 > $log_path 2>&1
--- dy2static/unittest/reinforcement_learning/train.sh
+++ dy2static/unittest/reinforcement_learning/train.sh
@@ -4,4 +4,4 @@ model="reinforcement_learning"
 
 log_path="./${model}.log"
 
-python model.py --device=GPU --pass_num=360 --gamma=0.99 --log_interval=10 > $log_path 2>&1 &
+python model.py --device=GPU --pass_num=360 --gamma=0.99 --log_interval=10 > $log_path 2>&1
