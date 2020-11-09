a7ce943a12af8bc8388079bb0f9da3f0d2801467
mmglove <gmm_email@163.com>
2020-11-09 17:23:35 +0800

    fix rec ce

--- ce/python3.7_linux_gpu_train_slim/PaddleRec_repo/.run_ce.sh
+++ ce/python3.7_linux_gpu_train_slim/PaddleRec_repo/.run_ce.sh
@@ -14,49 +14,137 @@ else
 fi
 }
 python setup.py install
-# contentunderstanding
+# 1 contentunderstanding
 run_con_cpu(){
-cp ${dataset_path}/rec_config/$1_cpu_config.yaml ./
+cp ${dataset_path}/rec_repo/rec_config/$1_cpu_config.yaml ./
 python -m paddlerec.run -m ./$1_cpu_config.yaml >${log_path}/$1_cpu 2>&1
 #print_info $? $1
 }
 run_con_gpu(){
-cp ${dataset_path}/rec_config/$1_gpu_config.yaml ./
+cp ${dataset_path}/rec_repo/rec_config/$1_gpu_config.yaml ./
 CUDA_VISIBLE_DEVICES=$cudaid1 python -m paddlerec.run -m ./$1_gpu_config.yaml >${log_path}/$1_gpu1 2>&1
 #print_info $? $1
 }
 # 1.1 tagspace
 model=tagspace
 cd ${current_dir}/models/contentunderstanding/tagspace
-ln -s ${dataset_path}/textcnn/senta_data senta_data
+mv data data_bk
+ln -s ${dataset_path}/rec_repo/tagspace/data
 run_con_cpu ${model}
 run_con_gpu ${model}
 cd ${current_dir}
-cat ${log_path}/${model}_cpu |grep done |head -10|tail -1 |awk -F ' |,|=' '{print ""'${model}'"_epoch_time_cpu\t"$7"\t"'${model}'"_acc_cpu\t"$12"\t"'${model}'"_loss_cpu\t"$15}'|tr -d '[][]' |python _ce.py
-cat ${log_path}/${model}_gpu1 |grep done |head -10|tail -1 |awk -F ' |,|=' '{print ""'${model}'"_epoch_time_gpu1\t"$7"\t"'${model}'"_acc_gpu1\t"$12"\t"'${model}'"_loss_gpu1\t"$15}'|tr -d '[][]' |python _ce.py
+cat ${log_path}/${model}_cpu |grep done |head -10|tail -1 |awk -F ' |,|=' '{print "kpis\t""'${model}'""_epoch_time_cpu\t"$7"\nkpis\t""'${model}'""_acc_cpu\t"$12"\nkpis\t""'${model}'""_loss_cpu\t"$15}'|tr -d '[][]' |python _ce.py
+cat ${log_path}/${model}_gpu1 |grep done |head -10|tail -1 |awk -F ' |,|=' '{print "kpis\t""'${model}'""_epoch_time_gpu1\t"$7"\nkpis\t""'${model}'""_acc_gpu1\t"$12"\nkpis\t""'${model}'""_loss_gpu1\t"$15}'|tr -d '[][]' |python _ce.py
 
 # 1.2 textcnn
 model=textcnn
 cd ${current_dir}/models/contentunderstanding/textcnn
-ln -s ${dataset_path}/textcnn/senta_data senta_data
+ln -s ${dataset_path}/rec_repo/textcnn/senta_data
 run_con_cpu ${model}
 run_con_gpu ${model}
 cd ${current_dir}
-cat ${log_path}/${model}_cpu |grep done |head -16|tail -1 |awk -F ' |,|=' '{print ""'${model}'"_epoch_time_cpu\t"$7"\t"'${model}'"_acc_cpu\t"$12"\t"'${model}'"_loss_cpu\t"$15}'|tr -d '[][]' |python _ce.py
-cat ${log_path}/${model}_gpu1 |grep done |head -16|tail -1 |awk -F ' |,|=' '{print ""'${model}'"_epoch_time_gpu1\t"$7"\t"'${model}'"_acc_gpu1\t"$12"\t"'${model}'"_loss_gpu1\t"$15}'|tr -d '[][]' |python _ce.py
+cat ${log_path}/${model}_cpu |grep done |head -16|tail -1 |awk -F ' |,|=' '{print "kpis\t""'${model}'""_epoch_time_cpu\t"$7"\nkpis\t""'${model}'""_acc_cpu\t"$12"\t""'${model}'""_loss_cpu\t"$15}'|tr -d '[][]' |python _ce.py
+cat ${log_path}/${model}_gpu1 |grep done |head -16|tail -1 |awk -F ' |,|=' '{print "kpis\t""'${model}'""_epoch_time_gpu1\t"$7"\nkpis\t""'${model}'""_acc_gpu1\t"$12"\t""'${model}'""_loss_gpu1\t"$15}'|tr -d '[][]' |python _ce.py
 
 
 # 1.3 textcnn_pretrain
 model=textcnn_pretrain
 cd ${current_dir}/models/contentunderstanding/textcnn_pretrain
-ln -s ${dataset_path}/textcnn/senta_data senta_data
+ln -s ${dataset_path}/rec_repo/textcnn/senta_data
+ln -s ${dataset_path}/rec_repo/textcnn/pretrain/pretrain_model
 run_con_cpu ${model}
 run_con_gpu ${model}
 cd ${current_dir}
-cat ${log_path}/${model}_cpu |grep done |head -6|tail -1 |awk -F ' |,|=' '{print ""'${model}'"_epoch_time_cpu\t"$7"\t"'${model}'"_acc_cpu\t"$12"\t"'${model}'"_loss_cpu\t"$15}'|tr -d '[][]' |python _ce.py
-cat ${log_path}/${model}_gpu1 |grep done |head -6|tail -1 |awk -F ' |,|=' '{print ""'${model}'"_epoch_time_gpu1\t"$7"\t"'${model}'"_acc_gpu1\t"$12"\t"'${model}'"_loss_gpu1\t"$15}'|tr -d '[][]' |python _ce.py
+cat ${log_path}/${model}_cpu |grep done |head -6|tail -1 |awk -F ' |,|=' '{print "kpis\t""'${model}'""_epoch_time_cpu\t"$7"\nkpis\t""'${model}'""_acc_cpu\t"$12"\nkpis\t""'${model}'""_loss_cpu\t"$15}'|tr -d '[][]' |python _ce.py
+cat ${log_path}/${model}_gpu1 |grep done |head -6|tail -1 |awk -F ' |,|=' '{print "kpis\t""'${model}'""_epoch_time_gpu1\t"$7"\nkpis\t""'${model}'""_acc_gpu1\t"$12"\nkpis\t""'${model}'""_loss_gpu1\t"$15}'|tr -d '[][]' |python _ce.py
 
 # 3.1 multitask (1/3)  esmm
-#model=esmm
-#run_con_cpu ${model}
-#cat ${log_path}/${model}_cpu |grep done |head -6|tail -1 |awk -F ' |,|=' '{print ""'${model}'"_epoch_time_cpu\t"$7"\t"'${model}'"_acc_cpu\t"$12"\t"'${model}'"_loss_cpu\t"$15}'|tr -d '[][]' |python _ce.py
+model=esmm
+cd ${current_dir}/models/multitask/esmm
+mv data data_bk
+ln -s ${dataset_path}/rec_repo/esmm/data
+run_con_cpu ${model}
+run_con_gpu ${model}
+cd ${current_dir}
+cat ${log_path}/${model}_cpu |grep done |head -6|tail -1 |awk -F ' |,|=' '{print "kpis\t""'${model}'""_epoch_time_cpu\t"$7"\nkpis\t"'${model}'"_acc_cpu\t"$12"\nkpis\t""'${model}'""_loss_cpu\t"$15}'|tr -d '[][]' |python _ce.py
+cat ${log_path}/${model}_gpu1 |grep done |head -6|tail -1 |awk -F ' |,|=' '{print "kpis\t""'${model}'""_epoch_time_gpu1\t"$7"\nkpis\t""'${model}'""_acc_gpu1\t"$12"\nkpis\t""'${model}'""_loss_gpu1\t"$15}'|tr -d '[][]' |python _ce.py
+
+# 3.2 mmoe
+model=mmoe
+cd ${current_dir}/models/multitask/mmoe
+mv data data_bk
+ln -s ${dataset_path}/rec_repo/mmoe/data
+run_con_cpu ${model}
+run_con_gpu ${model}
+cd ${current_dir}
+cat ${log_path}/${model}_cpu |grep done |head -6|tail -1 |awk -F ' |,|=' '{print "kpis\t""'${model}'""_epoch_time_cpu\t"$7"\nkpis\t""'${model}'""_acc_cpu\t"$12"\nkpis\t""'${model}'""_loss_cpu\t"$15}'|tr -d '[][]' |python _ce.py
+cat ${log_path}/${model}_gpu1 |grep done |head -6|tail -1 |awk -F ' |,|=' '{print "kpis\t""'${model}'""_epoch_time_gpu1\t"$7"\nkpis\t""'${model}'""_acc_gpu1\t"$12"\nkpis\t""'${model}'""_loss_gpu1\t"$15}'|tr -d '[][]' |python _ce.py
+
+# 4.1 rank(5) deepfm
+model=deepfm
+cd ${current_dir}/models/rank/deepfm
+mv data data_bk
+ln -s ${dataset_path}/rec_repo/deepfm/data
+run_con_cpu ${model}
+run_con_gpu ${model}
+cd ${current_dir}
+cat ${log_path}/${model}_cpu |grep done |head -1 |awk -F ' ' '{print "kpis\t""'${model}'""_epoch_time_cpu\t"$6}' |tr -d ','|python _ce.py
+cat ${log_path}/${model}_cpu |grep done |awk -F ',|=' 'END{print "kpis\t""'${model}'""_auc_cpu\t"$4}' |tr -d '[]'|python _ce.py
+cat ${log_path}/${model}_gpu1 |grep done |head -1 |awk -F ' ' '{print "kpis\t""'${model}'""_epoch_time_gpu1\t"$6}' |tr -d ','|python _ce.py
+cat ${log_path}/${model}_gpu1 |grep done |awk -F ',|=' 'END{print "kpis\t""'${model}'""_auc_gpu1\t"$4}' |tr -d '[]'|python _ce.py
+
+# 4.2 dnn
+model=dnn
+cd ${current_dir}/models/rank/dnn
+mv data data_bk
+ln -s ${dataset_path}/rec_repo/dnn/data
+run_con_cpu ${model}
+run_con_gpu ${model}
+cd ${current_dir}
+cat ${log_path}/${model}_cpu |grep done |head -1 |awk -F ' ' '{print "kpis\t""'${model}'""_epoch_time_cpu\t"$6}' |tr -d ','|python _ce.py
+cat ${log_path}/${model}_cpu |grep done |awk -F ',|=' 'END{print "kpis\t""'${model}'""_auc_cpu\t"$4}' |tr -d '[]'|python _ce.py
+cat ${log_path}/${model}_gpu1 |grep done |head -1 |awk -F ' ' '{print "kpis\t""'${model}'""_epoch_time_gpu1\t"$6}' |tr -d ','|python _ce.py
+cat ${log_path}/${model}_gpu1 |grep done |awk -F ',|=' 'END{print "kpis\t""'${model}'""_auc_gpu1\t"$4}' |tr -d '[]'|python _ce.py
+
+# 4.3 fm
+model=fm
+cd ${current_dir}/models/rank/${model}
+mv data data_bk
+ln -s ${dataset_path}/rec_repo/${model}/data
+run_con_cpu ${model}
+run_con_gpu ${model}
+cd ${current_dir}
+cat ${log_path}/${model}_cpu |grep done |head -1 |awk -F ' ' '{print "kpis\t""'${model}'""_epoch_time_cpu\t"$6}' |tr -d ','|python _ce.py
+cat ${log_path}/${model}_cpu |grep done |awk -F ',|=' 'END{print "kpis\t""'${model}'""_auc_cpu\t"$4}' |tr -d '[]'|python _ce.py
+cat ${log_path}/${model}_gpu1 |grep done |head -1 |awk -F ' ' '{print "kpis\t""'${model}'""_epoch_time_gpu1\t"$6}' |tr -d ','|python _ce.py
+cat ${log_path}/${model}_gpu1 |grep done |awk -F ',|=' 'END{print "kpis\t""'${model}'""_auc_gpu1"\t$4}' |tr -d '[]'|python _ce.py
+
+
+# 4.4 lr
+model=lr
+cd ${current_dir}/models/rank/${model}
+mv data data_bk
+ln -s ${dataset_path}/rec_repo/${model}/data
+run_con_cpu ${model}
+run_con_gpu ${model}
+cd ${current_dir}
+cat ${log_path}/${model}_cpu |grep done |head -1 |awk -F ' ' '{print "kpis\t""'${model}'""_epoch_time_cpu\t"$6}' |tr -d ','|python _ce.py
+cat ${log_path}/${model}_cpu |grep done |awk -F ',|=' 'END{print "kpis\t""'${model}'""_auc_cpu\t"$4}' |tr -d '[]'|python _ce.py
+cat ${log_path}/${model}_gpu1 |grep done |head -1 |awk -F ' ' '{print "kpis\t""'${model}'""_epoch_time_gpu1\t"$6}' |tr -d ','|python _ce.py
+cat ${log_path}/${model}_gpu1 |grep done |awk -F ',|=' 'END{print "kpis\t""'${model}'""_auc_gpu1\t"$4}' |tr -d '[]'|python _ce.py
+
+
+# 4.5 wide_deep
+model=wide_deep
+cd ${current_dir}/models/rank/${model}
+mv data data_bk
+ln -s ${dataset_path}/rec_repo/${model}/data
+run_con_cpu ${model}
+run_con_gpu ${model}
+cd ${current_dir}
+cat ${log_path}/${model}_cpu |grep done |head -1 |awk -F ' ' '{print "kpis\t""'${model}'""_epoch_time_cpu\t"$6}' |tr -d ','|python _ce.py
+cat ${log_path}/${model}_cpu |grep done |awk -F ',|=' 'END{print "kpis\t""'${model}'""_auc_cpu\t"$4}' |tr -d '[]'|python _ce.py
+cat ${log_path}/${model}_gpu1 |grep done |head -1 |awk -F ' ' '{print "kpis\t""'${model}'""_epoch_time_gpu1\t"$6}' |tr -d ','|python _ce.py
+cat ${log_path}/${model}_gpu1 |grep done |awk -F ',|=' 'END{print "kpis\t""'${model}'""_auc_gpu1\t"$4}' |tr -d '[]'|python _ce.py
+
+
