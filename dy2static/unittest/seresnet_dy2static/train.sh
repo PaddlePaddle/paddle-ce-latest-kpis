7d7423c8ac7291e48d0204ad8562a56463dd1ac9
lipeihan01 <lipeihan01@baidu.com>
2020-11-09 15:29:58 +0800

    [dy2static] modify train parameter

--- dy2static/unittest/seresnet_dy2static/train.sh
+++ dy2static/unittest/seresnet_dy2static/train.sh
@@ -4,4 +4,4 @@ model="seresnet"
 
 log_path="./${model}.log"
 
-python model.py --device=GPU --batch_size=8 --pass_num=5 --log_internal=10 > $log_path 2>&1
+python model.py --device=GPU --batch_size=8 --pass_num=20 --log_internal=10 > $log_path 2>&1
