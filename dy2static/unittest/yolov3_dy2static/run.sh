6edaca2552ed0ee0a10255d8f8508fbbf75c831e
Ray2020BD <563755780@qq.com>
2020-11-18 06:40:56 +0000

    Add yolov3 dy2static test for ce platform

--- dy2static/unittest/yolov3_dy2static/run.sh
+++ dy2static/unittest/yolov3_dy2static/run.sh
@@ -4,8 +4,8 @@ model="yolov3"
 
 log_path="./${model}.log"
 
-pip3 install pycocotools
+pip install pycocotools
 
-python3 download.py
+python download.py
 
-python3 model.py --device=GPU --batch_size=4 --pass_num=2 > $log_path 2>&1
+python model.py --device=GPU --batch_size=4 --pass_num=2 > $log_path 2>&1
