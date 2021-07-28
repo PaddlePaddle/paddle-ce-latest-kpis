1f501f392358fc2de1e9182abf3266dc722b2983
iamWHTWD <iamwhtwd@163.com>
2021-07-28 20:50:51 +0800

    update P0 case and delete rename log

--- ce_cloud_models/PaddleSlim/linux/scripts/slim_dy_quant_v1/train.sh
+++ ce_cloud_models/PaddleSlim/linux/scripts/slim_dy_quant_v1/train.sh
@@ -20,16 +20,13 @@ mkdir -p $log_path
 print_info(){
 if [ $1 -ne 0 ];then
     cat ${log_path}/$2.log
-    mv ${log_path}/$2.log ${log_path}/F_$2.log
     echo -e "\033[31m ${log_path}/F_$2 \033[0m"
+    echo "exit_code: 1.0" >>${log_path}/$2.log
 else
-#    cat ${log_path}/$2.log
-    mv ${log_path}/$2.log ${log_path}/S_$2.log
     echo -e "\033[32m ${log_path}/S_$2 \033[0m"
+    echo "exit_code: 0.0" >>${log_path}/$2.log
 fi
 }
-
-
 
 cd $code_path
 
